14d62c07d9c998473001616446b21cad114a4207
Andrew Crawford Foulkes <afoulkes@ucsd.edu>
2021-01-28 11:08:40 -0800

    lab4

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,21 @@
  * GET home page.
  */
 
-exports.view = function(req, res){
-  res.render('index');
+
+
+exports.view = function(req,res){
+	res.render('index', {
+		"projects": 
+		[
+		{
+			"name": "Waiting in Line",
+			"image": "dog.jpg",
+			"id": "project 1"
+		},
+		{
+			"name": "Waiting in Line",
+			"image": "dog.jpg",
+			"id": "project 1"
+		}]
+	});
 };
